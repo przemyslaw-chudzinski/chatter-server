45782624247960a4091da1e6a09a5001df12d6c8
Przemyslaw Chudzinski <przemek@lig-it.com>
2018-07-10 21:10:36 +0200

    commit

--- core/ws-server/ws-server-actions.js
+++ core/ws-server/ws-server-actions.js
@@ -1,5 +1,6 @@
 module.exports = {
     UserLogged: 'USER_LOGGED',
     UserLoggedOut: 'USER_LOGGED_OUT',
-    ContactStatusChanged: 'CONTACT_STATUS_CHANGED'
+    ContactStatusChanged: 'CONTACT_STATUS_CHANGED',
+    MessageToContact: 'MESSAGE_TO_CONTACT'
 }
