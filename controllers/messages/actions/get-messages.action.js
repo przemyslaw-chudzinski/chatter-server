b57107e9b91817e55d301fb12d4a77001a5cd65e
Przemyslaw Chudzinski <przemek@lig-it.com>
2018-07-19 22:23:24 +0200

    works

--- controllers/messages/actions/get-messages.action.js
+++ controllers/messages/actions/get-messages.action.js
@@ -13,9 +13,19 @@ class GetMessagesAction extends ActionBase {
 
     _init() {
         this._messagesModel.getMessages(this.loggedUserId, this._req.params.recipientId).then(data => {
-            this._res.status(200);
-            this._res.json(data);
-            console.log(data);
+            async.map(data.results, (item, next) => {
+                this._usersModel.getUserById(item.authorId).then(author => {
+                    item.author = author;
+                    next(false, item);
+                }).catch(err => next(err));
+            }, (err, results) => {
+                if (err) {
+                    return this.simpleResponse(500, 'Internal server error', err);
+                }
+                data.results = results;
+                this._res.status(200);
+                this._res.json(data);
+            });
         }).catch(err => this.simpleResponse(500, 'Internal server error', err));
     }
 }
