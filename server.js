0555d04b8d18166e76998a8bda4f670289ba8e87
Przemyslaw Chudzinski <przemek@lig-it.com>
2018-10-12 14:34:17 +0200

    before merge

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const wsServer = require('./core/ws-server');
 
 app.listen(serverConfig.port, () => console.log('Http Server is running on http://localhost:' + serverConfig.port));
 
-// const wsServer = new WebSocketServer();
-// wsServer.listen();
 wsServer.createServer();
-
-// console.log('server file', wsServer.getInstance())
 
 module.exports = app;
