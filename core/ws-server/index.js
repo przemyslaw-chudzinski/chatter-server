32f255815a8f30a3b12ea1a5a3376151b6c495cc
Przemyslaw Chudzinski <przemek@lig-it.com>
2018-10-12 17:04:22 +0200

    clean up

--- core/ws-server/index.js
+++ core/ws-server/index.js
@@ -2,8 +2,6 @@ const WebSocketServer = require('./ws-server');
 
 let wsInstance = null;
 
-// const getInstance = () => wsInstance;
-
 const getInstance = () => {
     if (!wsInstance) {
         wsInstance = new WebSocketServer();
@@ -11,15 +9,9 @@ const getInstance = () => {
     return wsInstance;
 };
 
-// const registerActions = (actions) => {
-//     wsInstance.register(actions);
-//     return wsInstance;
-// };
-
 const listen = () => wsInstance.listen();
 
 module.exports = {
     getInstance,
-    // registerActions,
     listen
 };
