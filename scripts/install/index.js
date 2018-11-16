d9c93afa5479b6831177f50618e96d359233baf9
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2018-11-16 20:09:08 +0100

    working on install script

--- scripts/install/index.js
+++ scripts/install/index.js
@@ -1,3 +1,5 @@
+const db = require('../../db');
+const collections = require('../../db/collections/index');
 const PasswordEncryption = require('../../core/password-encryption/index');
 const PasswordEncryptionBaseStrategy = require('../../core/password-encryption/password-encryption-base-strategy');
 
@@ -5,4 +7,25 @@ const passwordEncryption = new PasswordEncryption(PasswordEncryptionBaseStrategy
 
 const password = passwordEncryption.encode('123456');
 
-console.log(password);
+
+db.dbDriver.openConnection((err, client, db) => {
+
+    if (err) {
+        return console.log('connection error');
+    }
+
+    db.collection(collections.USERS).insert({
+        firstName: 'Przemysław',
+        lastName: 'Chudziński',
+        email: 'przemyslaw-chudzinski@wp.pl',
+        password: password,
+        confirmed: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        avatar: null
+    }, (err, doc) => {
+        client.close();
+        process.exit();
+    });
+
+});
