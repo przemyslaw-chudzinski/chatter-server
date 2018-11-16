6c3e61737c7ed6cda6ee442b844901e4902fa0ae
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2018-11-16 20:05:16 +0100

    working on install script

--- scripts/install/index.js
+++ scripts/install/index.js
@@ -1,1 +1,8 @@
-console.log('install')
+const PasswordEncryption = require('../../core/password-encryption/index');
+const PasswordEncryptionBaseStrategy = require('../../core/password-encryption/password-encryption-base-strategy');
+
+const passwordEncryption = new PasswordEncryption(PasswordEncryptionBaseStrategy);
+
+const password = passwordEncryption.encode('123456');
+
+console.log(password);
